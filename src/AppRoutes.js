12c13dfb0e85062ebab7123d3bbd83aa53288199
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-11 19:31:12 +0530

    changes done

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
-import { Calculator } from './Pages/Calculator'
-import { Index } from './Pages/Index'
+import { BackgroundColorChanger } from "./Pages/BackgroundColorChanger";
+import { Calculator } from "./Pages/Calculator";
+import { Index } from "./Pages/Index";
 
-export const AppRoutes = () =>{
-    return (
-
-    )
-}
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/bgcolor/:colorname" element={<BackgroundColorChanger />} />
+      <Route path="/Calculator" element={<Calculator />} />
+      <Route path="/" element={<Index />} />
+    </Routes>
+  );
+};
--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-
-export const BackgroundColorChanger = () =>{
-    return(
-       
-    )
-}
+export const BackgroundColorChanger = () => {
+  let { colorname } = useParams();
+  return (
+    <main
+      className="bgcolorchange"
+      style={{ height: "100vh", backgroundColor: `${colorname}` }}
+    >
+      Background Color Changer
+    </main>
+  );
+};
